fdab46ec36907b4d6b18fd64611e0ba7f5af1135
dpwdec <declankolakowski@gmail.com>
2020-04-24 08:52:36 +0100

    completed testing relationships between classes

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -18,15 +18,34 @@ require_relative '../lib/secret_diary'
 ### WARNING ###
 
 RSpec.describe SecretDiary do
-  context "when locked" do
-    pending "refuses to be read"
 
-    pending "refuses to be written"
+  let(:diary) { double() }
+  let(:subject) { described_class.new(diary) }
+  let(:message) { String.new("") }
+
+  context "when locked" do
+    it "refuses to be read" do
+      expect(diary).to_not receive(:read)
+      subject.read
+    end
+
+    it "refuses to be written" do
+      expect(diary).to_not receive(:write)
+      subject.write(message)
+    end
   end
 
   context "when unlocked" do
-    pending "gets read"
+    it "gets read" do
+      expect(diary).to receive(:read)
+      subject.unlock
+      subject.read
+    end
 
-    pending "gets written"
+    it "gets written" do
+      expect(diary).to receive(:write)
+      subject.unlock
+      subject.write(message)
+    end
   end
 end
